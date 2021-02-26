b184e5a5f67e6ab51ba48daf34213e3b67d6f4e4
grzegorz <grzegorzklimek77@gmail.com>
2021-02-26 15:37:36 +0100

    add files to list

--- script.py
+++ script.py
@@ -1,11 +1,16 @@
 import os
+import pandas as pd
 
 pathToData="/media/frezer02/7D3CDB1B28552965/Data/bert/"
 
+listOfFilesToMerge=[]
+
 for dirName in os.listdir(pathToData):
     subDirectory = os.path.join(pathToData,dirName)
-    # print(subDirectory)
     for fileName in os.listdir(subDirectory):
         if "street" in fileName or "stop-and-search" in fileName: 
-          print(fileName)
+          pathToFile = os.path.join(subDirectory,fileName)
+          listOfFilesToMerge.append(pathToFile)
 
+for item in listOfFilesToMerge:
+    print(item)
