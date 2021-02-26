30a200529448a2548f471441ce26bb31d111e0f4
grzegorz <grzegorzklimek77@gmail.com>
2021-02-26 15:58:14 +0100

    script is ready

--- script.py
+++ script.py
@@ -1,16 +1,16 @@
 import os
 import pandas as pd
 
-pathToData="/media/frezer02/7D3CDB1B28552965/Data/bert/"
+pathToData="/media/frezer02/7D3CDB1B28552965/Data/bert/police"
 
 listOfFilesToMerge=[]
 
 for dirName in os.listdir(pathToData):
     subDirectory = os.path.join(pathToData,dirName)
     for fileName in os.listdir(subDirectory):
-        if "street" in fileName or "stop-and-search" in fileName: 
+        if "street" in fileName: 
           pathToFile = os.path.join(subDirectory,fileName)
           listOfFilesToMerge.append(pathToFile)
 
-for item in listOfFilesToMerge:
-    print(item)
+combined_csv = pd.concat([pd.read_csv(f) for f in listOfFilesToMerge ])
+combined_csv.to_csv( "/media/frezer02/7D3CDB1B28552965/Data/bert/street.csv", index=False, encoding='utf-8-sig')
