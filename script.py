445ee9580814a7c0aae3068dc57c863055f6ff06
grzegorz <grzegorzklimek77@gmail.com>
2021-02-26 16:54:48 +0100

    use variables for specifying target instead of hard code

--- script.py
+++ script.py
@@ -1,16 +1,19 @@
 import os
 import pandas as pd
 
-pathToData="/media/frezer02/7D3CDB1B28552965/Data/bert/police"
-
+pathToDataDir="/media/frezer02/7D3CDB1B28552965/Data/bert/"
+searchedType="street.csv"
+dataDir="police_test"
 listOfFilesToMerge=[]
+dataDir=pathToDataDir + "/" + dataDir
+combined_csv_file_path = pathToDataDir + "/" + searchedType
 
-for dirName in os.listdir(pathToData):
-    subDirectory = os.path.join(pathToData,dirName)
+for dirName in os.listdir(dataDir):
+    subDirectory = os.path.join(dataDir + "/" + dirName)
     for fileName in os.listdir(subDirectory):
-        if "street" in fileName: 
-          pathToFile = os.path.join(subDirectory,fileName)
-          listOfFilesToMerge.append(pathToFile)
+        if searchedType in fileName:
+            pathToFile = os.path.join(subDirectory,fileName)
+            listOfFilesToMerge.append(pathToFile)
 
 combined_csv = pd.concat([pd.read_csv(f) for f in listOfFilesToMerge ])
-combined_csv.to_csv( "/media/frezer02/7D3CDB1B28552965/Data/bert/street.csv", index=False, encoding='utf-8-sig')
+combined_csv.to_csv( combined_csv_file_path, index=False, encoding='utf-8-sig')
