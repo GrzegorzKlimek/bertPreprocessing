e73b8fd2cba2d2cb63d708d2a2919ec267e0fb6f
grzegorz <grzegorzklimek77@gmail.com>
2021-03-01 14:01:16 +0100

    merge file by file

--- script.py
+++ script.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 pathToDataDir="/media/frezer02/7D3CDB1B28552965/Data/bert/"
 searchedType="street.csv"
-dataDir="police_test"
+dataDir="police"
 listOfFilesToMerge=[]
 dataDir=pathToDataDir + "/" + dataDir
 combined_csv_file_path = pathToDataDir + "/" + searchedType
@@ -14,6 +14,14 @@ for dirName in os.listdir(dataDir):
         if searchedType in fileName:
             pathToFile = os.path.join(subDirectory,fileName)
             listOfFilesToMerge.append(pathToFile)
+print("found list of files to merge")
+combined_csv = pd.read_csv(listOfFilesToMerge[0])
+del listOfFilesToMerge[0]
+for f in listOfFilesToMerge:
+    print("processing file: " + f)
+    try:
+        combined_csv = pd.concat([pd.read_csv(f), combined_csv ])
+    except Exception:
+        print("for file " + f + " there was exception: " + str(Exception))
 
-combined_csv = pd.concat([pd.read_csv(f) for f in listOfFilesToMerge ])
 combined_csv.to_csv( combined_csv_file_path, index=False, encoding='utf-8-sig')
