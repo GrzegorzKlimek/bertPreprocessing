29f4b2496e011d2ca99a93b49385de5329cce65e
grzegorz <grzegorzklimek77@gmail.com>
2021-03-02 08:24:19 +0100

    merge files by directories first then merge output files together

--- script.py
+++ script.py
@@ -1,27 +1,40 @@
 import os
 import pandas as pd
 
-pathToDataDir="/media/frezer02/7D3CDB1B28552965/Data/bert/"
-searchedType="street.csv"
-dataDir="police"
-listOfFilesToMerge=[]
+
+def filesToMerge(dirPath, substringName):
+    listOfFilesToMerge = []
+    for fileName in os.listdir(dirPath):
+        if substringName in fileName:
+            pathToFile = os.path.join(dirPath, fileName)
+            listOfFilesToMerge.append(pathToFile)
+    print("found list of files to merge")
+    return listOfFilesToMerge
+
+
+def mergeFiles(listOfFiles, target):
+    combined_csv_file_path = target
+    combined_csv = pd.read_csv(listOfFiles[0])
+    del listOfFiles[0]
+    for f in listOfFiles:
+        print("processing file: " + f)
+        try:
+            combined_csv = pd.concat([pd.read_csv(f), combined_csv])
+        except Exception:
+            print("for file " + f + " there was exception: " + str(Exception))
+    combined_csv.to_csv(combined_csv_file_path, index=False, encoding='utf-8-sig')
+
+
+pathToDataDir = "/media/frezer02/7D3CDB1B28552965/Data/bert/"
+targetDir = pathToDataDir + "output/"
+searchedType = "street.csv"
+dataDir = "police_test"
 dataDir=pathToDataDir + "/" + dataDir
-combined_csv_file_path = pathToDataDir + "/" + searchedType
 
 for dirName in os.listdir(dataDir):
     subDirectory = os.path.join(dataDir + "/" + dirName)
-    for fileName in os.listdir(subDirectory):
-        if searchedType in fileName:
-            pathToFile = os.path.join(subDirectory,fileName)
-            listOfFilesToMerge.append(pathToFile)
-print("found list of files to merge")
-combined_csv = pd.read_csv(listOfFilesToMerge[0])
-del listOfFilesToMerge[0]
-for f in listOfFilesToMerge:
-    print("processing file: " + f)
-    try:
-        combined_csv = pd.concat([pd.read_csv(f), combined_csv ])
-    except Exception:
-        print("for file " + f + " there was exception: " + str(Exception))
+    listOfFilesToMerge = filesToMerge(subDirectory, searchedType)
+    mergeFiles(listOfFilesToMerge, targetDir + dirName + "-" + searchedType )
 
-combined_csv.to_csv( combined_csv_file_path, index=False, encoding='utf-8-sig')
+listOfFilesToMerge = filesToMerge(targetDir,searchedType)
+mergeFiles(listOfFilesToMerge, targetDir + searchedType)
